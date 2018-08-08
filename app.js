4395c1a5f6320b20eb2a79345bc63b82c6a38ac9
olivier <rodomond.o@gmail.com>
2018-08-08 18:37:40 +0700

    lint

--- app.js
+++ app.js
@@ -1,73 +1,71 @@
-const config = require('./config'),
-      express = require('express'),
-      low = require('lowdb'),
-      FileSync = require('lowdb/adapters/FileSync'),
-      apicache = require('apicache'),
-      Instagram = require('node-instagram').default;
+const config = require('./config')
+const express = require('express')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const apicache = require('apicache')
+const Instagram = require('node-instagram').default
 
+const instagram = new Instagram(config.instagram)
+const adapter = new FileSync(config.dbFile)
+const db = low(adapter)
 
-const instagram = new Instagram(config.instagram);
-const adapter = new FileSync(config.dbFile);
-const db = low(adapter);
+db.defaults({config: {}, user: {}}).write()
 
-db.defaults({ config: {}, user: {}}).write();
-
-const app = express()
+express()
   .use((req, res, next) => {
-    req.accessToken = db.get('config').value().accessToken;
+    req.accessToken = db.get('config').value().accessToken
     if (req.url.match(/^\/auth/) || req.accessToken) {
-      return next();
+      return next()
     }
-    res.redirect('/auth/instagram');
+    res.redirect('/auth/instagram')
   })
   .get('/auth/instagram', (req, res) => {
     const options = {
-      scope: ['basic', 'likes'],
-    };
-    res.redirect(instagram.getAuthorizationUrl(config.redirectUri, options));
+      scope: ['basic', 'likes']
+    }
+    res.redirect(instagram.getAuthorizationUrl(config.redirectUri, options))
   })
   .get('/auth/instagram/callback', async (req, res) => {
     try {
-      const code = req.query.code;
-      const data = await instagram.authorizeUser(code, config.redirectUri);
-      db.set('config.accessToken', data.access_token).write();
-      db.set('user', data.user).write();
-      res.redirect('/');
+      const code = req.query.code
+      const data = await instagram.authorizeUser(code, config.redirectUri)
+      db.set('config.accessToken', data.access_token).write()
+      db.set('user', data.user).write()
+      res.redirect('/')
     } catch (err) {
-      res.json(err);
+      res.json(err)
     }
   })
   .get('/posts', apicache.middleware('1 minute'), async (req, res, next) => {
     try {
       const options = {
-        count : config.numberPerPage,
-        max_id : req.query.p,
-        access_token : req.accessToken
-      };
-      const posts = await instagram.get('users/self/media/recent', options);
-      delete posts.pagination.next_url;
+        count: config.numberPerPage,
+        max_id: req.query.p,
+        access_token: req.accessToken
+      }
+      const posts = await instagram.get('users/self/media/recent', options)
+      delete posts.pagination.next_url
       posts.data = posts.data
         .filter(post => {
-          if (!config.hashtag)  return true;
-          return post.tags.includes(config.hashtag.replace('#',''));
+          if (!config.hashtag) return true
+          return post.tags.includes(config.hashtag.replace('#', ''))
         })
         .map(post => {
           return {
-            id : post.id,
-            images : post.images,
-            caption : post.caption,
-            tags : post.tags,
-            type : post.type,
-            link : post.link
+            id: post.id,
+            images: post.images,
+            caption: post.caption,
+            tags: post.tags,
+            type: post.type,
+            link: post.link
           }
-        });
+        })
       res.json(posts)
     } catch (e) {
-      next(e);
+      next(e)
     }
   })
   .use(express.static('public'))
   .listen(config.port, () => {
-    console.log('App listning on port :' + config.port);
-  });
-
+    console.log('App listning on port :' + config.port)
+  })
--- config.js
+++ config.js
@@ -1,11 +1,13 @@
+const path = require('path')
+
 module.exports = {
-  dbFile: __dirname + '/db.json',
-  port : process.env.PORT || 8080,
-  redirectUri : (process.env.HOST || '') + '/auth/instagram/callback',
-  instagram : {
+  dbFile: path.resolve(__dirname, '/db.json'),
+  port: process.env.PORT || 8080,
+  redirectUri: (process.env.HOST || '') + '/auth/instagram/callback',
+  instagram: {
     clientId: process.env.INSTA_CLIENT_ID,
-    clientSecret: process.env.INSTA_CLIENT_SECRET,
+    clientSecret: process.env.INSTA_CLIENT_SECRET
   },
-  hashtag : process.env.HASHTAG,
-  numberPerPage: process.env.NUMBER_PER_PAGE,
-};
+  hashtag: process.env.HASHTAG,
+  numberPerPage: process.env.NUMBER_PER_PAGE
+}
