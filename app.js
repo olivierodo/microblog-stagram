e6bb17278a4a388790a51da4985ec313ec966c7b
olivier <rodomond.o@gmail.com>
2018-08-08 18:17:14 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const config = require('./config'),
+      express = require('express'),
+      low = require('lowdb'),
+      FileSync = require('lowdb/adapters/FileSync'),
+      apicache = require('apicache'),
+      Instagram = require('node-instagram').default;
+
+
+const instagram = new Instagram(config.instagram);
+const adapter = new FileSync(config.dbFile);
+const db = low(adapter);
+
+db.defaults({ config: {}, user: {}}).write();
+
+const app = express()
+  .use((req, res, next) => {
+    req.accessToken = db.get('config').value().accessToken;
+    if (req.url.match(/^\/auth/) || req.accessToken) {
+      return next();
+    }
+    res.redirect('/auth/instagram');
+  })
+  .get('/auth/instagram', (req, res) => {
+    const options = {
+      scope: ['basic', 'likes'],
+    };
+    res.redirect(instagram.getAuthorizationUrl(config.redirectUri, options));
+  })
+  .get('/auth/instagram/callback', async (req, res) => {
+    try {
+      const code = req.query.code;
+      const data = await instagram.authorizeUser(code, config.redirectUri);
+      db.set('config.accessToken', data.access_token).write();
+      db.set('user', data.user).write();
+      res.redirect('/');
+    } catch (err) {
+      res.json(err);
+    }
+  })
+  .get('/posts', apicache.middleware('1 minute'), async (req, res, next) => {
+    try {
+      const options = {
+        count : config.numberPerPage,
+        max_id : req.query.p,
+        access_token : req.accessToken
+      };
+      const posts = await instagram.get('users/self/media/recent', options);
+      delete posts.pagination.next_url;
+      posts.data = posts.data
+        .filter(post => {
+          if (!config.hashtag)  return true;
+          return post.tags.includes(config.hashtag.replace('#',''));
+        })
+        .map(post => {
+          return {
+            id : post.id,
+            images : post.images,
+            caption : post.caption,
+            tags : post.tags,
+            type : post.type,
+            link : post.link
+          }
+        });
+      res.json(posts)
+    } catch (e) {
+      next(e);
+    }
+  })
+  .use(express.static('public'))
+  .listen(config.port, () => {
+    console.log('App listning on port :' + config.port);
+  });
+
---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dbFile: __dirname + '/db.json',
+  port : process.env.PORT || 8080,
+  redirectUri : (process.env.HOST || '') + '/auth/instagram/callback',
+  instagram : {
+    clientId: process.env.INSTA_CLIENT_ID,
+    clientSecret: process.env.INSTA_CLIENT_SECRET,
+  },
+  hashtag : process.env.HASHTAG,
+  numberPerPage: process.env.NUMBER_PER_PAGE,
+};
